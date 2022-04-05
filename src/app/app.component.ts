105c85b02718e2a248af3759080f694dd46017cd
Waikato_Ethan <>
2022-04-05 13:17:52 +1200

    Add HTML, CSS and logic components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+// import { randomInt } from 'crypto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+// store member state
 export class AppComponent {
-  title = 'team-generator';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | '' = '';
+  // Nested array
+  teams: string[][] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+  onNumberOfTeamsInput(value: string) {
+    this.numberOfTeams = Number(value);
+  }
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Empty? Input something';
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
+  generateTeams() {
+    this.teams = [];
+    const allMembers = [...this.members];
+
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMessage = 'Invalid number of teams';
+      return;
+    }
+
+    this.errorMessage = '';
+
+    if (this.numberOfTeams > this.members.length) {
+      this.errorMessage = 'Greater than the number of members';
+      return;
+    }
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex = Math.floor(Math.random());
+        // splice() return an array, and remove what it returned
+        const member = allMembers.splice(randomIndex, 1)[0];
+        if (!member) break;
+        // If the team does not exist, then create one.
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+        this.members = [];
+      }
+    }
+  }
 }
